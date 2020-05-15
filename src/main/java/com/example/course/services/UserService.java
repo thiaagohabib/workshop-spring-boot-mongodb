df2a2e543c4cefbf0300250bfb507f01112cfff4
Thiago Habib <thiago.habib56@gmail.com>
2020-05-14 21:48:43 -0300

    Obtendo um usuário por id

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.course.domain.User;
 import com.example.course.repository.UserRepository;
+import com.example.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,8 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+}
 }
