381a20b720b0cee4f6fa17f361290892dc2bfd74
Thiago Habib <thiago.habib56@gmail.com>
2020-05-17 15:46:04 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/example/course/domain/Post.java
+++ src/main/java/com/example/course/domain/Post.java
@@ -1,12 +1,15 @@
 package com.example.course.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.example.course.dto.AuthorDTO;
+import com.example.course.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+			
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -72,6 +77,13 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 	
 	@Override
 	public int hashCode() {
@@ -97,6 +109,8 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 
 	
 }
